a37bd7b53c5f306a237cf97f8c4d616ed9d20aff
Daniel Vera <dlv04c@spear-13.rcc.fsu.edu>
2015-10-26 14:25:48 -0400

    added recentering to narrowPeakToBed

--- R/narrowPeakToBed.R
+++ R/narrowPeakToBed.R
@@ -1,14 +1,32 @@
-narrowPeakToBed <- function ( narrowPeaks , strand = TRUE ){
+narrowPeakToBed <- function ( narrowPeaks , recenter=TRUE , strand = TRUE , threads=getOption("threads",1L) ){
 
-	outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , ".bed" )
+	outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , "_recentered.bed" )
 
-	for(i in 1:length(narrowPeaks)){
-		np <- read.tsv ( narrowPeaks[i] )
-		if(ncol(np)<6 & strand){ strand = FALSE }
-		np[which(np[,5] > 1000),5] <- 1000
-		bed <- cbind ( np[,1:4] , round( np[,5] ) , if(strand){np[,6]} else{"+"} , np[,2] + np[,10] , np[,2] + np[,10] + 1 , stringsAsFactors = FALSE )
-		write.tsv( bed , file = outnames[i] )
-	}
+	dump <- mclapply(1:length(narrowPeaks), function(i){
+		# np <- read.tsv ( narrowPeaks[i] )
+		# if(ncol(np)<6 & strand){ strand = FALSE }
+		# np[which(np[,5] > 1000),5] <- 1000
+		# bed <- cbind ( np[,1:4] , round( np[,5] ) , if(strand){np[,6]} else{"+"} , np[,2] + np[,10] , np[,2] + np[,10] + 1 , stringsAsFactors = FALSE )
+		# write.tsv( bed , file = outnames[i] )
+		#
+
+
+		headlines.call<-pipe(paste("head",narrowPeaks[i],"| awk '{print $10}'"),open="r")
+		headlines<-as.numeric(readLines(headlines.call))
+		close(headlines.call)
+
+		if(length(which(headlines %in% (-1)>0))){
+			if(getOption("verbose")){ cat("bad peak coordinate detected in narrowPeak file, treating as bed\n") }
+			outnames[i] <- narrowPeaks[i]
+		} else{
+			if(getOption("verbose")){ cat("peak coordinates detected in narrowPeak file, recentering bed features\n") }
+			cmdString <- paste("awk '{$2=$2+$10;$3=$2+1;print}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
+			res <- cmdRun(cmdString)
+		}
+
+
+
+	}, mc.cores=threads,mc.preschedule=FALSE)
 
 	return(outnames)
 }
