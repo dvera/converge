54329f11d4a252c916fc4b232d3fd94caf3e99ec
Daniel Vera <dlv04c@spear-49.local>
2016-02-18 16:11:47 -0500

    various

--- R/narrowPeakToBed.R
+++ R/narrowPeakToBed.R
@@ -1,4 +1,4 @@
-narrowPeakToBed <- function ( narrowPeaks , recenter=TRUE , strand = TRUE , threads=getOption("threads",1L) ){
+narrowPeakToBed <- function ( narrowPeaks , recenter=FALSE , strand = TRUE , threads=getOption("threads",1L) ){
 
 	outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , "_recentered.bed" )
 
@@ -19,8 +19,12 @@ narrowPeakToBed <- function ( narrowPeaks , recenter=TRUE , strand = TRUE , thre
 			if(getOption("verbose")){ cat("bad peak coordinate detected in narrowPeak file, treating as bed\n") }
 			outnames[i] <- narrowPeaks[i]
 		} else{
-			if(getOption("verbose")){ cat("peak coordinates detected in narrowPeak file, recentering bed features\n") }
-			cmdString <- paste("awk '{$2=$2+$10;$3=$2+1;print}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
+			if(recenter){
+				if(getOption("verbose")){ cat("peak coordinates detected in narrowPeak file, recentering bed features\n") }
+				cmdString <- paste("awk '{$2=$2+$10;$3=$2+1;print}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
+			} else{
+				cmdString <- paste("awk '{if($5>1000){$5=1000}; print $1,$2,$3,$4,$5,$6,$2+$10,$2+$10+1}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
+			}
 			res <- cmdRun(cmdString)
 		}
 
