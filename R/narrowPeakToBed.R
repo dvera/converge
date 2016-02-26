1059e4a5d9f6461d3083f471d51c13459211cdca
Daniel Vera <dlv04c@spear-14.rcc.fsu.edu>
2016-02-26 14:12:33 -0500

    updated np2bed

--- R/narrowPeakToBed.R
+++ R/narrowPeakToBed.R
@@ -1,36 +1,49 @@
-narrowPeakToBed <- function ( narrowPeaks , recenter=FALSE , strand = TRUE , threads=getOption("threads",1L) ){
+narrowPeakToBed <- function ( narrowPeaks , recenter=FALSE , strand = TRUE , recenteredSize = 100 , threads=getOption("threads",1L) ){
 
-	outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , "_recentered.bed" )
+	# make sure input files aren't beds which would cause clobbering
+	if(any(file_ext(narrowPeaks)=="bed"){stop("at least one input file is a bed file")}
 
-	dump <- mclapply(1:length(narrowPeaks), function(i){
-		# np <- read.tsv ( narrowPeaks[i] )
-		# if(ncol(np)<6 & strand){ strand = FALSE }
-		# np[which(np[,5] > 1000),5] <- 1000
-		# bed <- cbind ( np[,1:4] , round( np[,5] ) , if(strand){np[,6]} else{"+"} , np[,2] + np[,10] , np[,2] + np[,10] + 1 , stringsAsFactors = FALSE )
-		# write.tsv( bed , file = outnames[i] )
-		#
+	numfiles <- length(narrowPeaks)
 
+	# define output names
+	if(recenter){
+		outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , "_recentered.bed" )
+	} else{
+		outnames <- paste0( basename ( removeext ( narrowPeaks ) ) , ".bed" )
+	}
 
-		headlines.call<-pipe(paste("head",narrowPeaks[i],"| awk '{print $10}'"),open="r")
-		headlines<-as.numeric(readLines(headlines.call))
-		close(headlines.call)
+	# create commands to check for peak coordinates
+	cmdString<-paste("head",narrowPeaks,"| awk '{print $10}'")
 
-		if(length(which(headlines %in% (-1)>0))){
-			if(getOption("verbose")){ cat("bad peak coordinate detected in narrowPeak file, treating as bed\n") }
-			outnames[i] <- narrowPeaks[i]
-		} else{
-			if(recenter){
-				if(getOption("verbose")){ cat("peak coordinates detected in narrowPeak file, recentering bed features\n") }
-				cmdString <- paste("awk '{$2=$2+$10;$3=$2+1;print}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
-			} else{
-				cmdString <- paste("awk '{if($5>1000){$5=1000}; print $1,$2,$3,$4,$5,$6,$2+$10,$2+$10+1}' OFS='\t' ",narrowPeaks[i]," | sort -k1,1 -k2,2n > ",outnames[i],sep="")
-			}
-			res <- cmdRun(cmdString)
+	# get peak info for first 10 lines
+	res <- as.numeric(unlist(cmdRun(cmdString,lines=T)))
+
+	# find files with no peak info
+	nopeaks <- which(res<0)
+	arenopeaks <- length(nopeaks)>0
+
+	# if there are files without peak info and recentering is requested
+	if(arenopeaks & recenter){
+		if(getOption("verbose")){
+			cat("WARNING: no peak coordinate detected in the following files, treating as bed:",paste(narrowPeaks[nopeaks],collapse=" "),"\n")
 		}
+	}
 
+	halfsize <- round(recenteredSize/2)
 
+	midString <- rep("",numfiles)
 
-	}, mc.cores=threads,mc.preschedule=FALSE)
+	if(arenopeaks){
+		midString[nopeaks] <- "$10=int(($3-$2)/2);"
+	}
+
+	if(recenter){
+		cmdString <- paste("awk '{",midstring,"print $1,$2+$10-",halfsize,",$2+$10+",halfsize,",$4,int($5),$6,$2+$10,$2+$10+1}' OFS='\t' ",narrowPeaks," | sort -k1,1 -k2,2n > ",outnames)
+	} else{
+		cmdString <- paste("awk '{",midstring,"print $1,$2,$3,$4,int($5),$6,$2+$10,$2+$10+1}' OFS='\t' ",narrowPeaks," | sort -k1,1 -k2,2n > ",outnames)
+	}
+
+	res <- cmdRun(cmdString)
 
 	return(outnames)
 }
