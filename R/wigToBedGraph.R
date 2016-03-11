1e8be3391260beaae235b6b0e356cc5219caf7fe
Daniel Vera <dlv04c@spear-50.local>
2016-03-11 09:38:52 -0500

    wigToBedGraph runs

--- R/wigToBedGraph.R
+++ R/wigToBedGraph.R
@@ -1,9 +1,13 @@
-wigToBedGraph <- function(wiggleFiles){
+wigToBedGraph <- function(wiggleFiles, threads=getOption("threads",1L) ){
 
   outnames <- paste0(basename(removeext(wiggleFiles)),".bg")
 
   cmdString <- paste(
     "awk \'BEGIN{FS=\"\t\"} {if($1~\"Step\"){split($0,chrom,\"chrom=\"); split(chrom[2],chrom2,\" \");split($0,winsize,\"span=\"); split(winsize[2],winsize2,\" \")} else if(NF==2){print chrom2[1],$1,$1+winsize2[1],$2}}\' OFS=\'\t\'",wiggleFiles,">",outnames
   )
+
+  res <- cmdRun(cmdString, threads=threads)
+
   return(outnames)
+  
 }
